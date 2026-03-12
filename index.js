961df61aba5254ba0d8c25016939f8c9f3d755d8
git-rajudas <rajudas.coder@gmail.com>
2026-03-12 21:23:27 +0530

    MongoDB Connection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require('cookie-parser');
-
 
 const { connectToMongoDB } = require("./connect")
 const URL = require("./modules/url");
